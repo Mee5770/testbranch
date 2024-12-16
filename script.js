9d701c4840fc7496a89ac562707c2a36f1beb503
Mee5770 <rattapoommee01@gmail.com>
2024-12-16 09:38:39 +0700

    new test_v3

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
-console.iog("Rattapoom PhanTong")
-console.log("Mee")
+const display = document.getElementById("display");
+
+// เคลียร์หน้าจอ
+function clearDisplay() {
+  display.value = "";
+}
+
+// ลบตัวสุดท้าย
+function backspace() {
+  display.value = display.value.slice(0, -1);
+}
+
+// เพิ่มตัวเลขหรือเครื่องหมายที่กด
+function appendToDisplay(value) {
+  display.value += value;
+}
+
+// คำนวณผลลัพธ์
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = "Error";
+  }
+}
